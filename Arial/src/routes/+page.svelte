3f6369b98bc94bc5168226afc723608ff030ff0a
MaxMph <hurleymax255@gmail.com>
2026-06-16 10:26:05 -0400

    i probably should have commited by now

---
+++ Arial/src/routes/+page.svelte
@@ -0,0 +1,97 @@
+<div class="main_centered">
+
+<!-- <div style= "width: 100px; margin-left: auto; margin-right: auto;"> -->
+<pre>
+         /\/\
+        /  \ \
+       / /\ \ \
+       \/ /\/ /
+       / /\/ /\
+      / /\ \/\ \
+     / / /\ \ \ \
+  /\/ / / /\ \ \ \/\
+ /  \/ / /  \ \ \ \ \
+/ /\ \/ /    \ \/\ \ \
+\/ /\/ /      \/ /\/ /
+/ /\/ /\      / /\/ /\
+\ \ \/\ \    / /\ \/ /
+ \ \ \ \ \  / / /\  /
+  \/\ \ \ \/ / / /\/
+     \ \ \ \/ / /
+      \ \/\ \/ /
+       \/ /\/ /
+       / /\/ /\
+       \ \ \/ /
+        \ \  /
+         \/\/
+</pre>
+<!-- </div> -->
+
+<div style="">
+<h2>&lt;:::::::::::::]==() HELVETICA ()==[:::::::::::::&gt;</h2>
+<h3>Make a text only game, get a book or keyboard!</h3>
+<p style="color:rgba(255, 255, 255, 0.6);">-+H+-+H+-+H+-+H+-</p>
+<p>In games like Zork, Rouge, and dwarf fortress, game developers have used text as a visual medium to great effect, now you try it out yourself!</p>
+<a href="/info">More Info</a>
+<a href="https://forms.hackclub.com/t/nY9z3h4d7zus">Submit</a>
+</div>
+
+<pre>
+         /\/\
+        /  \ \
+       / /\ \ \
+       \/ /\/ /
+       / /\/ /\
+      / /\ \/\ \
+     / / /\ \ \ \
+  /\/ / / /\ \ \ \/\
+ /  \/ / /  \ \ \ \ \
+/ /\ \/ /    \ \/\ \ \
+\/ /\/ /      \/ /\/ /
+/ /\/ /\      / /\/ /\
+\ \ \/\ \    / /\ \/ /
+ \ \ \ \ \  / / /\  /
+  \/\ \ \ \/ / / /\/
+     \ \ \ \/ / /
+      \ \/\ \/ /
+       \/ /\/ /
+       / /\/ /\
+       \ \ \/ /
+        \ \  /
+         \/\/
+</pre>
+</div>
+
+
+<style>
+
+pre {
+    font-family: "Courier New", monospace;
+    text-align: left;
+    font-size: small;
+    color:rgba(255, 255, 255, 0.6);
+}
+
+.main_centered{
+    font-family:monospace ;
+    color: white;
+    background: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* height: 100vh;  */
+    text-align: center;
+    flex-direction: column;
+    height: 98vh;
+    
+}
+
+h1{
+
+}
+
+p{
+
+}
+
+</style>
---
+++ Arial/src/routes/info/+page.svelte
@@ -0,0 +1,103 @@
+<div class="main_centered">
+
+<!-- <div style= "width: 100px; margin-left: auto; margin-right: auto;"> -->
+<pre>
+         /\/\
+        /  \ \
+       / /\ \ \
+       \/ /\/ /
+       / /\/ /\
+      / /\ \/\ \
+     / / /\ \ \ \
+  /\/ / / /\ \ \ \/\
+ /  \/ / /  \ \ \ \ \
+/ /\ \/ /    \ \/\ \ \
+\/ /\/ /      \/ /\/ /
+/ /\/ /\      / /\/ /\
+\ \ \/\ \    / /\ \/ /
+ \ \ \ \ \  / / /\  /
+  \/\ \ \ \/ / / /\/
+     \ \ \ \/ / /
+      \ \/\ \/ /
+       \/ /\/ /
+       / /\/ /\
+       \ \ \/ /
+        \ \  /
+         \/\/
+</pre>
+<!-- </div> -->
+
+<div style="">
+<h2>&lt;:::::::::::::]==() HELVETICA ()==[:::::::::::::&gt;</h2>
+<h3>Make a text only game, get a book audible membership, or ereader!</h3>
+<p style="color:rgba(255, 255, 255, 0.6);">-+H+-+H+-+H+-+H+-</p>
+<p>
+    Code for 3 hours, get an audible membership/credit or a book grant (local book stores encouraged!) <br>
+    Code for 16 hours and get the <a href="https://www.xteink.com/products/xteink-x4">Xteink X4 Pocket eReader!</a>
+    <br> <br> Track your work using Hackatime. No double dipping. You must be 18 or younger to participate!
+
+</p>
+
+<a href="/">Return</a>
+<a href="https://forms.hackclub.com/t/nY9z3h4d7zus">Submit</a>
+</div>
+
+<pre>
+         /\/\
+        /  \ \
+       / /\ \ \
+       \/ /\/ /
+       / /\/ /\
+      / /\ \/\ \
+     / / /\ \ \ \
+  /\/ / / /\ \ \ \/\
+ /  \/ / /  \ \ \ \ \
+/ /\ \/ /    \ \/\ \ \
+\/ /\/ /      \/ /\/ /
+/ /\/ /\      / /\/ /\
+\ \ \/\ \    / /\ \/ /
+ \ \ \ \ \  / / /\  /
+  \/\ \ \ \/ / / /\/
+     \ \ \ \/ / /
+      \ \/\ \/ /
+       \/ /\/ /
+       / /\/ /\
+       \ \ \/ /
+        \ \  /
+         \/\/
+</pre>
+</div>
+
+
+<style>
+
+pre {
+    font-family: "Courier New", monospace;
+    text-align: left;
+    font-size: small;
+    color:rgba(255, 255, 255, 0.6);
+}
+
+.main_centered{
+    font-family:monospace ;
+    color: white;
+    background: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* height: 100vh;  */
+    text-align: center;
+    flex-direction: column;
+    height: 98vh;
+    
+}
+
+h1{
+
+}
+
+p{
+
+}
+
+</style>
