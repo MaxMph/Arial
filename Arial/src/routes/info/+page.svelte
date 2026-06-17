715828419c23cd6839fe70dd92c7de23e4c3e088
MaxMph <hurleymax255@gmail.com>
2026-06-17 13:37:13 -0400

    blaaaa

--- Arial/src/routes/info/+page.svelte
+++ Arial/src/routes/info/+page.svelte
@@ -33,12 +33,15 @@
 <h3>Make a text only game, get a book, Audible membership, or ereader!</h3>
 <p style="color:rgba(255, 255, 255, 0.6);">-+H+-+H+-+H+-+H+-</p>
 <p>
-    Code for 1 hour to get an exclusive sticker! <br>
-    Code for 3 hours, get an audible membership/credit or a book grant (local book stores encouraged!) <br>
+    Ship any project to get an exclusive sticker! <br>
+    <!-- Code for 1 hour to get a $5 dollar book grant! (stackable)  -->
+    Code for 3 hours, get an audible membership/credit or a $15 book grant (local book stores encouraged!) <br>
     Code for 12 hours and get the <a href="https://www.xteink.com/products/xteink-x4">Xteink X4 Pocket eReader!</a>
     <br> <br> Track your work using Hackatime. No double dipping. You must be 18 or younger to participate!
     <br> AI is allowed while coding but it cant make up the majority of the project. No AI art is allowed!
     <br> Pushing the prompt of text only game is great, I would love to see something I wouldnt have expected from the prompt!!
+    <br> For submissions, Either provide an itch.io link or a deployed website link. if you made it in the terminal please provide a youtube video demo and make sure its simple to run!!
+    <br> If you have any questions, ask them in the slack channel below!
 
 </p>
 
