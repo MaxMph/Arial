24f54399bbb26e2699aad658fa6ffcd2b1e1bb15
MaxMph <hurleymax255@gmail.com>
2026-06-16 20:48:47 -0400

    more text and favicon

--- Arial/src/routes/+page.svelte
+++ Arial/src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<!-- <div class="paper-grain"> -->
+<!-- <div class="grain"> -->
+<div class="grain">
 <div class="main_centered">
 
 
@@ -31,7 +32,7 @@
 
 <div style="">
 <h2>&lt;:::::::::::::]==() HELVETICA ()==[:::::::::::::&gt;</h2>
-<h3>Make a text only game, get a book Audible membership, or ereader!</h3>
+<h3>Make a text only game, get a book, Audible membership, or ereader!</h3>
 <p style="color:rgba(255, 255, 255, 0.6);">-+H+-+H+-+H+-+H+-</p>
 <p>In games like Zork, Rouge, and dwarf fortress, game developers have used text as a visual medium to great effect, now you try it out yourself!</p>
 <a href="/info">More Info</a>
@@ -64,6 +65,7 @@
 </pre>
 </div>
 <!-- </div> -->
+</div>
 
 
 <style>
--- Arial/src/routes/info/+page.svelte
+++ Arial/src/routes/info/+page.svelte
@@ -1,3 +1,4 @@
+<div class="grain">
 <div class="main_centered">
 
 <!-- <div style= "width: 100px; margin-left: auto; margin-right: auto;"> -->
@@ -29,19 +30,21 @@
 
 <div style="">
 <h2>&lt;:::::::::::::]==() HELVETICA ()==[:::::::::::::&gt;</h2>
-<h3>Make a text only game, get a book Audible membership, or ereader!</h3>
+<h3>Make a text only game, get a book, Audible membership, or ereader!</h3>
 <p style="color:rgba(255, 255, 255, 0.6);">-+H+-+H+-+H+-+H+-</p>
 <p>
     Code for 1 hour to get a custom sticker! <br>
     Code for 3 hours, get an audible membership/credit or a book grant (local book stores encouraged!) <br>
     Code for 16 hours and get the <a href="https://www.xteink.com/products/xteink-x4">Xteink X4 Pocket eReader!</a>
     <br> <br> Track your work using Hackatime. No double dipping. You must be 18 or younger to participate!
+    <br> AI is allowed while coding but it cant make up the majority of the project. No AI art is allowed!
     <br> Pushing the prompt of text only game is great, I would love to see something I wouldnt have expected from the prompt!!
 
 </p>
 
 <a href="/">Return</a>
 <a href="https://forms.hackclub.com/t/nY9z3h4d7zus">Submit</a>
+<br> <a href="https://hackclub.enterprise.slack.com/archives/C0BB07QGR6E">#Helvetica</a>
 </div>
 
 <pre>
@@ -68,6 +71,7 @@
         \ \  /
          \/\/
 </pre>
+</div>
 </div>
 
 
